9bd559ac364d1a9392e48ec0147b77ea250b129e
Gabriel-Sembenelli <gasembenelli@gmail.com>
2023-05-22 17:56:26 -0300

    first

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+typedef long long   ll;
+
+#define dbg(var)    cout<<#var<<"="<<var<<" "
+#define dbgv(var)   cout<<#var<<"= ";for(auto e:var)cout<<e<<" ";cout<<endl
+#define endl        "\n"
+
+const int    INF    = 0x3f3f3f3f;
+const ll     LINF   = 0x3f3f3f3f3f3f3f3f;
+const int    MOD    = 1e9+7;
+
+using namespace std;
+
+/*
+
+*/
+
+void solve(int test_case=1){
+    
+}
+
+int main(){
+    //ios_base::sync_with_stdio(0);cin.tie(0);
+    int t=1;
+    cin>>t;
+    for(int i=1; i<=t; i++){
+        solve(i);
+    }
+    
+    
+    return 0;
+}

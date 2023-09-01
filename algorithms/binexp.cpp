a326faeb3271f67abf1fa288fb40efe3128904e6
Gabriel-Sembenelli <gasembenelli@gmail.com>
2023-08-31 21:02:23 -0300

    updated binexp

--- algorithms/binexp.cpp
+++ algorithms/binexp.cpp
@@ -1,12 +1,8 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-
 const int MOD = 1e9+7;
 
 long long binexpMOD(long long a, long long n){
     if(n==0) return 1;
-    long long ret = binexp(a, n/2);
+    long long ret = binexpMOD(a, n/2);
     if(n%2) return ((ret%MOD)*(ret%MOD)*(a%MOD))%MOD;
     return ((ret%MOD)*(ret%MOD))%MOD;
 }

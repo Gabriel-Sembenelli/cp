ac52a2f1d6330a81efc22ff1fd6748bad22f3ba5
Gabriel-Sembenelli <gasembenelli@gmail.com>
2023-08-31 21:02:14 -0300

    updated template

--- template.cpp
+++ template.cpp
@@ -1,19 +1,15 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 #define endl        "\n"
-#define dbg(V)      cout<<#V<<"="<<V<<" "
+#define dbg(V)      cout<<#V<<"= "<<V<<" "
 #define dbgv(V)     cout<<#V<<"=";for(auto e:V)cout<<" "<<e;cout<<endl
 #define int         long long
 
 const int    INF    = 0x3f3f3f3f;
-const int    LINF   = 0x3f3f3f3f3f3f3f3f;
+const int    LINF   = 0x3f3f3f3f3f3f3f3fLL;
 const int    MOD    = 1e9+7;
 
-using namespace std;
-
-/*
-
-*/
 
 void solve(){
     

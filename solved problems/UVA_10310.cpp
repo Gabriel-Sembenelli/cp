e9cfacd533ab157b474edfe9545bc2025781345f
Gabriel-Sembenelli <gasembenelli@gmail.com>
2023-11-08 13:57:36 -0300

    added Geometry solved problem

---
+++ solved problems/UVA_10310.cpp
@@ -0,0 +1,44 @@
+/*
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=9
+
+Geometry
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<double, double> point;
+#define X first
+#define Y second
+
+double dist(point p, point q){
+    return sqrt((p.X - q.X)*(p.X - q.X) + (p.Y - q.Y)*(p.Y - q.Y));
+}
+
+void solve(){
+    int n;
+    cout << setprecision(3) << fixed;
+    while(cin >> n){
+        bool flag = false;
+        point gop, dog;
+        cin >> gop.X >> gop.Y >> dog.X >> dog.Y;
+        for(int i=0; i<n; i++){
+            point hole;
+            cin >> hole.X >> hole.Y;
+            double dist_gop = dist(gop, hole);
+            double dist_dog = dist(dog, hole);
+            if(!flag and 2*dist_gop <= dist_dog){
+                cout << "The gopher can escape through the hole at ("
+                << hole.X << "," << hole.Y << ").\n";
+                flag = true;
+            }
+        }
+        if(!flag) cout << "The gopher cannot escape.\n";
+    }
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    //int t; cin>>t; for(int i=1; i<=t; i++)
+    solve();
+    return 0;
+}

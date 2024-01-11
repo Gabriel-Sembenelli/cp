b9b17bd3da883d1a5b8c72185f1c3addd1d08993
Gabriel-Sembenelli <gasembenelli@gmail.com>
2024-01-11 15:32:46 -0300

    updated template

--- template.cpp
+++ template.cpp
@@ -1,21 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endl "\n"
-#define dbg(V)  cerr<<#V<<"= "<<V<<" "
-#define dbgv(V) cerr<<#V<<"=";for(auto e:V)cerr<<" "<<e;cerr<<endl
-#define dbgm(V) cerr<<#V<<"=";for(auto [f,s]:V)cerr<<" {"<<f<<","<<s<<"},";cerr<<endl
-#define sz(x) (int)x.size()
-#define int long long
-#define ld long double
+#define int     long long
+#define ld      long double
+#define endl    "\n"
+#define sz(x)   (int)size(x)
+#define PB      push_back
+#define MP      make_pair
+#define all(x)  begin(x), end(x)
 
-const int INF = 0x3f3f3f3f3f3f3f3fLL;
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+const long long INF = 0x3f3f3f3f3f3f3f3fLL;
+
+int nxt(){ int x; cin >> x; return x; }
+
 
 void solve(){
     
 }
 
 signed main(){
+    //freopen(".in", "r", stdin);
+    //freopen(".out", "w", stdout);
     ios_base::sync_with_stdio(0);cin.tie(0);
     //int t; cin>>t; while(t--)
     solve();

157056da836cea0c7258aac60facfe7aa04d3d77
Gabriel-Sembenelli <gasembenelli@gmail.com>
2024-01-23 14:17:14 -0300

    updated cpp template

--- template.cpp
+++ template.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
 #define int     long long
 #define ld      long double
 #define endl    "\n"
@@ -8,14 +14,10 @@ using namespace std;
 #define MP      make_pair
 #define sz(x)   (int)size(x)
 #define all(x)  begin(x), end(x)
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define rall(x) rbegin(x), rend(x)
 
 const long long INF = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1); // or use M_PI from "cmath"
 
 int nxt(){ int x; cin >> x; return x; }
 
@@ -27,7 +29,7 @@ void solve(){
 signed main(){
     //freopen(".in", "r", stdin);
     //freopen(".out", "w", stdout);
-    ios_base::sync_with_stdio(0);cin.tie(0);
+    //ios_base::sync_with_stdio(0);cin.tie(0);
     //int t; cin>>t; while(t--)
     solve();
     

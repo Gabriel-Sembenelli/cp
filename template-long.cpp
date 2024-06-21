a83d22551bc9cf9d30e5c70af8e8f939e2535b7c
Gabriel-Sembenelli <gasembenelli@gmail.com>
2024-06-21 20:34:12 -0300

    att

--- template-long.cpp
+++ template-long.cpp
@@ -21,6 +21,24 @@ template<typename Head, typename... Tail> void read_in(Head &H, Tail&... T) { ci
 #define read(...) read_in(__VA_ARGS__)
 
 
+
+template <int D, typename T>
+struct Vec : public vector<Vec<D - 1, T>> {
+    static_assert(D >= 1, "Dimension must be positive");
+    template <typename... Args>
+    Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...)) {}
+};
+template <typename T>
+struct Vec<1, T> : public vector<T> {
+    Vec(int n = 0, T val = T()) : std::vector<T>(n, val) {}
+};
+/* Example
+    Vec<4, int64_t> f(n, k, 2, 2); // = f[n][k][2][2];
+    Vec<2, int> adj(n); // graph
+*/
+
+
+
 #define int         long long
 #define ld          long double
 #define sz(x)       (int)size(x)

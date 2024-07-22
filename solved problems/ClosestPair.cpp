b50e67b739cc1df0c3c83311cb4646fc65bf72c3
Gabriel Sembenelli <66696757+Gabriel-Sembenelli@users.noreply.github.com>
2024-07-22 15:44:00 -0300

    Update ClosestPair.cpp

--- solved problems/ClosestPair.cpp
+++ solved problems/ClosestPair.cpp
@@ -4,8 +4,8 @@ using namespace std;
 #define sz(V) (int)(V).size()
 #define all(V) begin(V),end(V)
 
-using T = int;
-using vi = vector<int>;
+using T = long double;
+using vT = vector<T>;
 
 struct Point {
     T x, y;
@@ -14,13 +14,15 @@ struct Point {
     Point(T _x, T _y, int _idx) : x(_x), y(_y), idx(_idx) {}
 };
 using Points = vector<Point>;
+using PP = pair<Point, Point>;
 bool x_cmp(Point &a, Point &b){ return (a.x != b.x) ? a.x < b.x : a.y <= b.y; }
 bool y_cmp(Point &a, Point &b){ return (a.y != b.y) ? a.y < b.y : a.x <= b.x; }
 T dist2(Point &a, Point &b){ return (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y); }
 
-
-pair<Point, Point> Brute(Points &X, int n){
-    pair<Point, Point> best = {X[0], X[1]};
+// Testa todos os (|X| choose 2) pares
+PP Brute(Points &X){
+    int n = sz(X);
+    PP best = {X[0], X[1]};
     for(int i=0; i<n-1; ++i)
         for(int j=i+1; j<n; ++j)
             if(dist2(X[i], X[j]) < dist2(best.first, best.second))
@@ -28,43 +30,36 @@ pair<Point, Point> Brute(Points &X, int n){
     return best;
 }
 
-pair<Point, Point> ClosestPairAux(Points &X, Points &Y){
+PP ClosestPairAux(Points &X, Points &Y){
     int n = sz(X);
     
     // Base
-    if(n <= 3) return Brute(X, n);
+    if(n <= 3) return Brute(X);
     
     // Divide
-    int mid = n/2; // bissect line
+    int mid = n/2;
     Points XL, XR, YL, YR;
     for(int i=0; i<n; ++i){
-        if(i <= mid)
-            XL.push_back(X[i]);
-        else
-            XR.push_back(X[i]);
-        if((Y[i].x < X[mid].x) or (Y[i].x == X[mid].x and Y[i].y <= X[mid].y))
-            YL.push_back(Y[i]);
-        else
-            YR.push_back(Y[i]);
+        if(i < mid) XL.push_back(X[i]);
+        else        XR.push_back(X[i]);
+        if((Y[i].x < X[mid].x) or (Y[i].x == X[mid].x and Y[i].y < X[mid].y))
+                YL.push_back(Y[i]);
+        else    YR.push_back(Y[i]);
     }
-    //cout << "XL:"; for(auto [x,y,i]:XL) cout<<" ("<<x<<","<<y<<","<<i<<")"; cout << '\n';
-    //cout << "XR:"; for(auto [x,y,i]:XR) cout<<" ("<<x<<","<<y<<","<<i<<")"; cout << '\n';
-    //cout << "YL:"; for(auto [x,y,i]:YL) cout<<" ("<<x<<","<<y<<","<<i<<")"; cout << '\n';
-    //cout << "YR:"; for(auto [x,y,i]:YR) cout<<" ("<<x<<","<<y<<","<<i<<")"; cout << "\n\n";
     
     // Conquer
-    pair<Point, Point> pL = ClosestPairAux(XL, YL);
-    pair<Point, Point> pR = ClosestPairAux(XR, YR);
-    T deltaL = dist2(pL.first, pL.second);
-    T deltaR = dist2(pR.first, pR.second);
-    T delta = min(deltaL, deltaR);
-    pair<Point, Point> best = pL;
-    if(delta == deltaR) best = pR;
+    PP closestL = ClosestPairAux(XL, YL);
+    PP closestR = ClosestPairAux(XR, YR);
+    T deltaL = dist2(closestL.first, closestL.second);
+    T deltaR = dist2(closestR.first, closestR.second);
+    T delta = min(deltaL, deltaR); // Ta ao quadrado!!
+    PP best = closestL;
+    if(delta == deltaR) best = closestR;
     
     // Combine
     Points Yrange;
     for(int i=0; i<n; ++i)
-        if(mid - delta <= Y[i].x and Y[i].x <= mid + delta)
+        if((X[mid].x - Y[i].x)*(X[mid].x - Y[i].x) <= delta) // Distancias ao quadrado!!
             Yrange.push_back(Y[i]);
     for(int i=0; i<sz(Yrange); ++i)
         for(int j = i+1; j <= min(i+7, sz(Yrange)-1); ++j)
@@ -74,7 +69,7 @@ pair<Point, Point> ClosestPairAux(Points &X, Points &Y){
     return best;
 }
 
-pair<int, int> ClosestPair(vi &X_coord, vi &Y_coord){
+pair<int, int> ClosestPair(vT &X_coord, vT &Y_coord){
     int n = sz(X_coord);
     Points X(n), Y(n);
     for(int i=0; i<n; ++i)
@@ -85,13 +80,13 @@ pair<int, int> ClosestPair(vi &X_coord, vi &Y_coord){
     for(int i=0; i<n-1; ++i)
         if(X[i].x == X[i+1].x and X[i].y == X[i+1].y)  // Caso trivial, distancia zero
             return {i, i+1};
-    pair<Point, Point> closest = ClosestPairAux(X, Y); // Caso nao trivial, usa D&C
+    PP closest = ClosestPairAux(X, Y); // Caso nao trivial, usa D&C
     return {closest.first.idx, closest.second.idx};
 }
 
 int main(){
     int n; cin >> n;
-    vi X_coord(n), Y_coord(n);
+    vT X_coord(n), Y_coord(n);
     for(int i=0; i<n; ++i)
         cin >> X_coord[i] >> Y_coord[i];
     pair<int, int> ans = ClosestPair(X_coord, Y_coord);

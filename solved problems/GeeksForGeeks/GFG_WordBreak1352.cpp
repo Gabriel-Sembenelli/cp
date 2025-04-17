fdeeb08a13aea71ba1d7e2dba59936bcc4191b9f
Gabriel Sembenelli <66696757+Gabriel-Sembenelli@users.noreply.github.com>
2025-04-16 23:48:52 -0300

    DP [Erickson] 3.3
    Atualizei a solução após notar mudanças no enunciado.

--- solved problems/GeeksForGeeks/GFG_WordBreak1352.cpp
+++ solved problems/GeeksForGeeks/GFG_WordBreak1352.cpp
@@ -1,11 +1,57 @@
 /*
-DP
-
+Topic: DP
+Problem: https://www.geeksforgeeks.org/problems/word-break1352/1
 References:
-[Er] Section 3.3
-https://jeffe.cs.illinois.edu/teaching/algorithms/book/03-dynprog.pdf
+  [Erickson] Section 3.3
+  https://jeffe.cs.illinois.edu/teaching/algorithms/book/03-dynprog.pdf
 */
 
+// VERSÃO NOVA DO PROBLEMA (ABRIL DE 2025)
+class Solution {
+public:
+    int dp[41234]{}; // Constraints estão erradas, s.size() passa de 30 mil
+    
+    // Iterativa (0.05s ~ 0.22s)
+    bool wordBreak(string &s, vector<string> &dictionary) {
+        unordered_set<string> dict;
+        for(auto w : dictionary) dict.insert(w);
+        int n = s.size();
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; --i){
+            dp[i] = 0;
+            string t = "";
+            for(int j = i; j < min(n, i + 100); ++j){
+                t += s[j];
+                dp[i] |= dp[j + 1] && dict.count(t); // AC  (0.05s)
+                //dp[i] |= dict.count(t) && dp[j + 1]; // AC (0.22s)
+                if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+    
+    // Recursiva (0.02s ~ 0.05s)
+    bool solve(string &s, unordered_set<string>& dict, int i){
+        if(dp[i] != -1) return dp[i];
+        int n = s.size();
+        if(i == n) return dp[i] = true;
+        string t = "";
+        for(int j = i; j < min(n, i + 100); j++){
+            t += s[j];
+            //if(solve(s, dict, j+1) && dict.count(t)) return dp[i] = true; // AC (0.05s)
+            if(dict.count(t) && solve(s, dict, j+1)) return dp[i] = true; // AC  (0.02s)
+        }
+        return dp[i] = false;
+    }
+    bool wordBreak(string &s, vector<string> &dictionary) {
+        unordered_set<string> dict;
+        for(auto w : dictionary) dict.insert(w);
+        for(int i=0; i <= s.size(); ++i) dp[i] = -1;
+        return solve(s, dict, 0);
+    }
+};
+
+// VERSÃO ANTIGA DO PROBLEMA (SETEMBRO DE 2024)
 class Solution {
 public:
     int dp[1123]{};
